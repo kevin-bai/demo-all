c9ebeca91aabbf0c0edef501add244e3ab1ff0f9
kevin-bai <897840029@qq.com>
2017-04-25 14:48:39 +0800

    图片用promise预加载

---
+++ promise_core/loadImage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/4/25.
+ */
+let loadImage = (url)=>{
+    return new Promise((resolve,reject)=>{
+        var image = new Image();
+        image.onload = ()=>{
+            console.log('onload');
+            resolve(image);
+        };
+        image.onerror = ()=>{
+            reject(new Error('cant load image'+image.src))
+        };
+        image.src = url;
+    })
+}
